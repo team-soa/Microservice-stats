f50559a1c71f64741b3b6c80040fd4bc19f079dc
Bratan <alfabrayan12@gmail.com>
2021-11-05 19:07:29 -0600

    Variable env de rabbit

--- src/configuration/app.ts
+++ src/configuration/app.ts
@@ -66,7 +66,7 @@ app.use(function(err:any, req:any, res:any, next:any) {
   res.render('error');
 });
 
-const rabbitHost = "amqp://localhost"
+const rabbitHost = "amqp://"+process.env.rabbit_url
 const connectionString =  'mongodb+srv://client:HzKRkF8M52TTjidj@cluster0.uaqcj.mongodb.net/test'
 const initializeStatisticsQueue = 'initializeStatistics'
 const updateStatisticsQueue = 'updateStatistics'
